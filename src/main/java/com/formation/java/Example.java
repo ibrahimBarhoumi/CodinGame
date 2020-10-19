e72eec45eb08d3b4d304d0344c649a8f2af6fc64
ibrahim_barhoumi <ibrahim_barhoumi@carrefour.com>
2020-10-20 00:56:40 +0200

    file finder soution

---
+++ src/main/java/com/formation/java/Example.java
@@ -0,0 +1,37 @@
+package com.formation.java;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * @author Admin
+ */
+public class Example {
+
+	private static String locateUniverseFormula() {
+		String name = "universe-formula";
+		File file = new File("/etc/documents");
+		String path = findFile(file, name);
+		System.out.println(path);
+		return path;
+	}
+
+	private static String findFile(File file, String name) {
+		String path = null;
+		File[] list = file.listFiles();
+		if (list != null) {
+			for(File fil : list)
+				if (fil.isDirectory()) {
+					findFile(fil, name);
+				} else if (name.equalsIgnoreCase(fil.getName())) {
+					path = fil.getAbsolutePath();
+					break;
+				}
+		}
+		return path;
+	}
+
+	public static void main(String[] args) {
+		locateUniverseFormula();
+	}
+}
